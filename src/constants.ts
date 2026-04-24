e299e42eeaec1a4ec56459c8b155539786bdc5b3
Manoj kumar <104956591+Manozharihar@users.noreply.github.com>
2026-04-24 15:11:57 +0530

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -56,7 +56,7 @@ export const MOCK_PARTS: Part[] = [
     id: "p1",
     name: "MacBook Pro 13\" (A2338) Battery",
     price: 100.00,
-    image: "https://laptopgallery.co.in/wp-content/uploads/2023/01/5.png?w=400",
+    image: "https://m.media-amazon.com/images/I/41tNBZuOqlL._SX300_SY300_QL70_FMwebp_.jpg?w=400",
     category: "Mobile & Electronics",
     compatibility: ["MacBook Pro 13\" M1", "MacBook Pro 13\" M2"],
     inStock: true,
